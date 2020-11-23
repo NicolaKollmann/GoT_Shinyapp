7858a2007afe9ee6d15f2245f44f9be78dd37e72
Nicola Kollmann <NicolaKollmann@users.noreply.github.com>
2020-11-23 20:53:15 +0100

    change the font to Trajan Pro

--- app.R
+++ app.R
@@ -36,28 +36,33 @@ characters <- sort(characters) # sort alphabetically
 ui <- fluidPage(theme = shinytheme("darkly"),
     
     # add the GitHub ribbon
-    ribbon_css("https://github.com/NicolaKollmann/Shinyapp", color = "#6edfb0"),
+    ribbon_css("https://github.com/NicolaKollmann/Shinyapp", color = "#6edfb0", link_css = list("font-family" = "GoT", "font-size" = "11px")),
+    
+    # use the GoT font Trajan Pro
+    tags$head(
+        includeCSS("www/CSS.css")
+    ),
                                         
     # Application title
-    titlePanel("Game of Thrones Wordcloud"),
-    h4(tags$a(href = "https://nkollmann.netlify.app/", "Nicola Kollmann")),
+    titlePanel(p(class = "p_Style", "Game of Thrones Wordcloud")),
+    h4(p(class = "p_Style", tags$a(href = "https://nkollmann.netlify.app/", "Nicola Kollmann"))),
 
     # Sidebar with a select input for the name of the character
     sidebarLayout(
         sidebarPanel(
-            selectInput("character",
+            div(selectInput("character",
                         "Choose a Character:",
-                        choices = characters),
+                        choices = characters), class = "p_Style"),
             hr(),
-            selectInput("shape",
+            div(selectInput("shape",
                         "Choose the Shape of the Wordcloud:",
-                        choices = c('circle', 'cardioid', 'diamond', 'triangle-forward', 'triangle', 'pentagon', 'star')),
+                        choices = c('circle', 'cardioid', 'diamond', 'triangle-forward', 'triangle', 'pentagon', 'star')), class = "p_Style"),
             hr(),
-            selectInput("pal",
+            div(selectInput("pal",
                         "Choose a Color Palette:",
-                        choices = c('Targaryen', 'Targaryen2', 'Stark', 'Stark2', 'Lannister', 'Martell', 'Tully', 'Greyjoy', 'Baratheon', 'Baratheon2', 'Tyrell', 'White_Walkers', 'Jon_Snow', 'Margaery', 'Daenerys', 'Game_of_Thrones', 'Wildfire', 'Arya')),
+                        choices = c('Targaryen', 'Targaryen2', 'Stark', 'Stark2', 'Lannister', 'Martell', 'Tully', 'Greyjoy', 'Baratheon', 'Baratheon2', 'Tyrell', 'White_Walkers', 'Jon_Snow', 'Margaery', 'Daenerys', 'Game_of_Thrones', 'Wildfire', 'Arya')), class = "p_Style"),
             hr(),
-            colourInput("col", "Background color", value = "white"),
+            div(colourInput("col", p(class = "p_Style", "Background color"), value = "white"), class = "p_Style"),
             hr(),
             
             # GoT Theme Song
@@ -68,16 +73,16 @@ ui <- fluidPage(theme = shinytheme("darkly"),
         mainPanel(
             
             tabsetPanel(type = "tabs",
-                        tabPanel("Wordcloud",
+                        tabPanel(p(class = "p_Style","Wordcloud"),
                                  wordcloud2Output("wordcloud2", width = "100%", height = "900")),
-                        tabPanel("Credits",
+                        tabPanel(p(class = "p_Style", "Credits"),
                                  br(),
-                                 p("Data: ", a(href="https://www.kaggle.com/albenft/game-of-thrones-script-all-seasons", "Kaggle")),
-                                 p("GoT Theme Song: ", a(href="https://mp3offline.org/uXZd_W5B7N0", "mp3offline.org")),
-                                 p("GoT Color Palettes: ", a(href="https://github.com/aljrico/gameofthrones", "Alejandro Jiménez")),
-                                 p("Wordcloud2: ", a(href="https://github.com/Lchiffon/wordcloud2", "Dawei Lang")),
-                                 p("GitHub Corner: ", a(href="https://github.com/colearendt/gitlink", "Cole Arendt")),
-                                 strong("Thank you very much!"),
+                                 p(class = "p_Style", "Data: ", a(href="https://www.kaggle.com/albenft/game-of-thrones-script-all-seasons", "Kaggle")),
+                                 p(class = "p_Style", "GoT Theme Song: ", a(href="https://mp3offline.org/uXZd_W5B7N0", "mp3offline.org")),
+                                 p(class = "p_Style", "GoT Color Palettes: ", a(href="https://github.com/aljrico/gameofthrones", "Alejandro Jiménez")),
+                                 p(class = "p_Style", "Wordcloud2: ", a(href="https://github.com/Lchiffon/wordcloud2", "Dawei Lang")),
+                                 p(class = "p_Style", "GitHub Corner: ", a(href="https://github.com/colearendt/gitlink", "Cole Arendt")),
+                                 strong(class = "p_Style", "Thank you very much!"),
                                  hr(),
                                  tags$iframe(src = "https://giphy.com/gifs/tk8aCAvTg8Hjq/html5", width = "300", height = "229")
                                  ),
@@ -110,7 +115,7 @@ server <- function(input, output) {
     output$wordcloud2 <- renderWordcloud2({
         
         # draw the wordcloud for the specified character
-        wordcloud2(wordcloud(), size = 0.7, shape = input$shape, color = pal(), backgroundColor = input$col)
+        wordcloud2(wordcloud(), fontFamily = 'GoT', size = 0.7, shape = input$shape, color = pal(), backgroundColor = input$col)
 
     })
 }
